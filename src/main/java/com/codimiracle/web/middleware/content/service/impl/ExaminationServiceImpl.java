df3bc32a895ddcb831b3153cdb6ce129abe0729b
codimiracle <codimiracle@outlook.com>
2020-04-21 11:55:17 +0800

    add post processor feature

--- src/main/java/com/codimiracle/web/middleware/content/service/impl/ExaminationServiceImpl.java
+++ src/main/java/com/codimiracle/web/middleware/content/service/impl/ExaminationServiceImpl.java
@@ -1,10 +1,9 @@
 package com.codimiracle.web.middleware.content.service.impl;
 
 import com.codimiracle.web.middleware.content.contract.AbstractService;
-import com.codimiracle.web.middleware.content.inflation.ExaminerInflatable;
+import com.codimiracle.web.middleware.content.extension.ExaminatedPostProcessor;
 import com.codimiracle.web.middleware.content.inflation.ExaminerInflater;
 import com.codimiracle.web.middleware.content.mapper.ExaminationMapper;
-import com.codimiracle.web.middleware.content.pojo.po.Content;
 import com.codimiracle.web.middleware.content.pojo.po.ContentArticle;
 import com.codimiracle.web.middleware.content.pojo.po.ContentExamination;
 import com.codimiracle.web.middleware.content.pojo.vo.ContentExaminationVO;
@@ -18,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Transactional
@@ -29,9 +29,14 @@ public class ExaminationServiceImpl extends AbstractService<String, ContentExami
     private ArticleService articleService;
     @Resource
     private ExaminationMapper examinationMapper;
+    @Autowired
+    private List<ExaminatedPostProcessor> examinatedPostProcessors;
 
     @Autowired(required = false)
     private ExaminerInflater examinerInflater;
+
+    public ExaminationServiceImpl() {
+    }
 
     @Override
     protected ContentExaminationVO mutate(ContentExaminationVO inflatedObject) {
@@ -53,18 +58,25 @@ public class ExaminationServiceImpl extends AbstractService<String, ContentExami
         if (Objects.isNull(article)) {
             throw new ServiceException("无法找到内容！");
         }
+        String fromStatus = article.getStatus();
+        // 更新评审后状态
         ContentArticle updatingArticle = new ContentArticle();
         updatingArticle.setStatus(toStatus);
         updatingArticle.setContentId(article.getContentId());
         articleService.update(updatingArticle);
+        // 回写状态
+        article.setStatus(toStatus);
+        //写入评审结果
         ContentExamination examination = new ContentExamination();
         examination.setReason(reason);
         examination.setTargetContentId(contentId);
         examination.setExaminedAt(new Date());
-        examination.setFromStatus(article.getStatus());
+        examination.setFromStatus(fromStatus);
         examination.setToStatus(toStatus);
         examination.setExaminerId(examinerId);
         save(examination);
+        // 发出评审结果
+        examinatedPostProcessors.forEach((listener) -> listener.onExaminated(article, fromStatus, toStatus));
     }
 
     @Override
@@ -92,5 +104,15 @@ public class ExaminationServiceImpl extends AbstractService<String, ContentExami
     @Override
     public void reject(String contentId, String examinerId, String reason) {
         examinate(contentId, examinerId, reason, ContentArticle.CONTENT_STATUS_REJECTED);
+    }
+
+    @Override
+    public void addPostProcessor(ExaminatedPostProcessor postProcessor) {
+        examinatedPostProcessors.add(postProcessor);
+    }
+
+    @Override
+    public void removePostProcessor(ExaminatedPostProcessor postProcessor) {
+        examinatedPostProcessors.remove(postProcessor);
     }
 }
